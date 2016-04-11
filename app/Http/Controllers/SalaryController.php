d89361341674c53bc5d22fb576b112eac7d6fa34
Abu Sayem <sayem@asteriskbd.com>
2016-04-11 11:11:48 +0600

    Complete Salary Generation

--- app/Http/Controllers/SalaryController.php
+++ app/Http/Controllers/SalaryController.php
@@ -55,39 +55,147 @@ class SalaryController extends Controller
         $noOfDaysInMonth = Carbon::create($request->year,$request->month)->daysInMonth;
 
         foreach ($employees as $employee){
-            /*get employee attendance register */
-            $attendances = Attendance::where('employee_id',$employee->id)
-                                        ->whereBetween('date',[$startDay,$endDay])
-                                        ->get();
-            //Count the attended date
-            $totalAttended = $attendances->count();
+            if($employee->status == 1)
+            {
+                /*get employee attendance register */
+                $attendances = Attendance::where('employee_id',$employee->id)
+                    ->whereBetween('date',[$startDay,$endDay])
+                    ->get();
+                //Count the attended date
+                $totalAttended = $attendances->count();
 
-            /*Employee's approved Leaves*/
-            $leaveDays = Leave::where('employee_id',$employee->id)
-                                ->whereBetween('start_day',[$startDay,$endDay])
-                                ->count();
-            /*Calculate Absent*/
-            $absent = $noOfDaysInMonth-($totalAttended+$leaveDays+$holidays);
-            /*Get number of Late attended days*/
-            $lateCount = Attendance::where('employee_id',$employee->id)
-                                ->whereBetween('date',[$startDay,$endDay])
-                                ->where('let_time','!=','00:00:00')
-                                ->count();
-            /*Get Employee's Salary Structure*/
-            $salaryStructure = SalaryStructure::where('employee_id',$employee->id)->first();
+                /*Employee's approved Leaves*/
+                $leaveDays = Leave::where('employee_id',$employee->id)
+                    ->whereBetween('start_day',[$startDay,$endDay])
+                    ->count();
+                /*Calculate Absent*/
+                $absent = $noOfDaysInMonth-($totalAttended+$leaveDays+$holidays);
+                /*Get number of Late attended days*/
+                $lateCount = Attendance::where('employee_id',$employee->id)
+                    ->whereBetween('date',[$startDay,$endDay])
+                    ->where('let_time','!=','00:00:00')
+                    ->count();
+                /*Get Employee's Salary Structure*/
+                $salaryStructure = SalaryStructure::where('employee_id',$employee->id)->first();
 
-            if($employee->type ==1){
-                if(!$absent && $lateCount<3){
-                    $bonusSetting = Setting::where('string','attendance_bonus')->first();
-                    $attendanceBonus = $bonusSetting->value;
-                }else{
-                    $attendanceBonus= 0;
+                if($employee->type ==1){
+                    if(!$absent && $lateCount<3){
+                        $bonusSetting = Setting::where('string','attendance_bonus')->first();
+                        $attendanceBonus = $bonusSetting->value;
+                    }else{
+                        $attendanceBonus= 0;
+                    }
+                }else {
+                    $attendanceBonus = 0;
                 }
-            }else {
-                $attendanceBonus = 0;
-            }
-            return $lateCount;
+                /*Calculate OT HOUR*/
+                $ot_hour = $attendances->sum('overtime');
 
+                $ot_amount = $ot_hour * $salaryStructure->ot_rate;
+
+                $salaryPerDay = $salaryStructure->basic/$noOfDaysInMonth;
+
+                $absentDeduction = $salaryPerDay*$absent;
+
+                $salaryRegister = new SalaryRegister();
+
+                $salaryRegister->employee_id = $employee->id;
+                $salaryRegister->salary_structure_id = $salaryStructure->id;
+                $salaryRegister->basic = $salaryStructure->basic;
+                $salaryRegister->house_rent = $salaryStructure->house_rent;
+                $salaryRegister->m_a = $salaryStructure->m_a;
+                $salaryRegister->f_a = $salaryStructure->f_a;
+                $salaryRegister->t_a = $salaryStructure->t_a;
+                $salaryRegister->gross = $salaryStructure->gross;
+                $salaryRegister->abs_days = $absent;
+                $salaryRegister->abs_deduction = $absentDeduction;
+                $salaryRegister->net_salary = $salaryStructure->gross - $absentDeduction;
+                $salaryRegister->att_bonus = $attendanceBonus;
+                $salaryRegister->ot_rate = $salaryStructure->ot_rate;
+                $salaryRegister->ot_hours = $ot_hour;
+                $salaryRegister->ot_amount = $ot_amount;
+                $salaryRegister->payable = $salaryRegister->net_salary+$attendanceBonus+$ot_amount;
+                $salaryRegister->adv_amount = 0;
+                $salaryRegister->stamp =10;
+                $salaryRegister->net_paid=0;
+                $salaryRegister->month = $request->month;
+                $salaryRegister->year = $request->year;
+
+                $salaryRegister->save();
+
+                echo 'if===';
+
+
+            } else{
+                /*get employee attendance register */
+                $attendances = Attendance::where('employee_id',$employee->id)
+                    ->whereBetween('date',[$startDay,$endDay])
+                    ->get();
+                //Count the attended date
+                $totalAttended = $attendances->count();
+
+                /*Employee's approved Leaves*/
+                $leaveDays = Leave::where('employee_id',$employee->id)
+                    ->whereBetween('start_day',[$startDay,$endDay])
+                    ->count();
+                /*Calculate Absent*/
+                $absent = $noOfDaysInMonth-($totalAttended+$leaveDays+$holidays);
+                /*Get number of Late attended days*/
+                $lateCount = Attendance::where('employee_id',$employee->id)
+                    ->whereBetween('date',[$startDay,$endDay])
+                    ->where('let_time','!=','00:00:00')
+                    ->count();
+                /*Get Employee's Salary Structure*/
+                $salaryStructure = SalaryStructure::where('employee_id',$employee->id)->first();
+
+                if($employee->type ==1){
+                    if(!$absent && $lateCount<3){
+                        $bonusSetting = Setting::where('string','attendance_bonus')->first();
+                        $attendanceBonus = $bonusSetting->value;
+                    }else{
+                        $attendanceBonus= 0;
+                    }
+                }else {
+                    $attendanceBonus = 0;
+                }
+                /*Calculate OT HOUR*/
+                $ot_hour = $attendances->sum('overtime');
+
+                $ot_amount = $ot_hour * $salaryStructure->ot_rate;
+
+                $salaryPerDay = $salaryStructure->gross/$noOfDaysInMonth;
+
+                $absentDeduction = 0;
+
+                $salaryRegister = new SalaryRegister();
+
+                $salaryRegister->employee_id = $employee->id;
+                $salaryRegister->salary_structure_id = $salaryStructure->id;
+                $salaryRegister->basic = $salaryStructure->basic;
+                $salaryRegister->house_rent = $salaryStructure->house_rent;
+                $salaryRegister->m_a = $salaryStructure->m_a;
+                $salaryRegister->f_a = $salaryStructure->f_a;
+                $salaryRegister->t_a = $salaryStructure->t_a;
+                $salaryRegister->gross = $salaryStructure->gross;
+                $salaryRegister->abs_days = $absent;
+                $salaryRegister->abs_deduction = $absentDeduction;
+                $salaryRegister->net_salary = $salaryPerDay*$totalAttended;
+                $salaryRegister->att_bonus = $attendanceBonus;
+                $salaryRegister->ot_rate = $salaryStructure->ot_rate;
+                $salaryRegister->ot_hours = $ot_hour;
+                $salaryRegister->ot_amount = $ot_amount;
+                $salaryRegister->payable = $salaryRegister->net_salary+$attendanceBonus+$ot_amount;
+                $salaryRegister->adv_amount = 0;
+                $salaryRegister->stamp =10;
+                $salaryRegister->net_paid=0;
+                $salaryRegister->month = $request->month;
+                $salaryRegister->year = $request->year;
+
+                $salaryRegister->save();
+
+                echo  $totalAttended;
+                echo "==else===";
+            }
         }
 
         return $noOfDaysInMonth;
