c6a252ae05a0f3b1fef437611099555f30a1f35a
Abu Sayem <sayem@asteriskbd.com>
2016-04-12 07:17:33 +0600

    Add dompdf library

--- config/app.php
+++ config/app.php
@@ -147,6 +147,8 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         Zofe\Rapyd\RapydServiceProvider::class,
         Aginev\Acl\AclServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
+
 
 
     ],
@@ -199,6 +201,7 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
